410c0c81173efa9ca80d10b9e8e8ffe84c373b44
DarrenLevene <darren@wilmington-consulting.co.uk>
2023-11-29 12:39:02 +0000

    Updating milestone_5.py to include docstrings

--- milestone_5.py
+++ milestone_5.py
@@ -1,7 +1,23 @@
 import random
 
 class Hangman:
+    '''
+    This class is used to represent a game of Hangman
+    Attributes:
+        word_list(list):        list of words from which the secret word to be guessed is selected.
+        word (str):             secret word to be guessed, which is selected at random from word_list.
+        word_guessed(list):     list of letters of the secret word, with "_" for each unguessed letter.
+        num_letters(int):       number of unique letters in the word that have not yet been guessed.
+        list_of_guesses(list):  list of guesses that have altready been tried.
+        num_lives(int):         number of lives the user has at the start of the game.
+
+    '''
     def __init__(self, word_list, num_lives=5):
+        '''
+        This is the constructor for the Hangman class.
+        Parameters are word_list and num_lives, which is set to a default value of 5.
+
+        '''
 
         self.word_list = word_list
         self.word = random.choice(self.word_list)
@@ -11,6 +27,12 @@ class Hangman:
         self.num_lives = num_lives
 
     def check_guess(self, guess):
+        '''
+        This method will check to see if the letter guessed by the user is contained within the secret word, selected at random within the list of words (word_list).
+        Arguments:  guess, which is a letter input guessed by the user.
+        Return: the method will print a message depending on whether the guess is contained within the secret word or not.
+
+        '''
         guess=guess.lower()
 
         if guess in self.word:
@@ -27,6 +49,13 @@ class Hangman:
             print(f"You have {self.num_lives} lives left.")
 
     def ask_for_input(self):
+        '''
+        This method will ask for user to guess a letter contained within the secret word, which they will input. 
+        It will test to see if the input is valid, or whether the letter had already been selected.
+        It will then check to see if letter is in the secret word.
+        Return: it will print out correct guesses in the appropriate positions
+
+        '''
         
         guess = input("Guess a letter:")                                          
         if len(guess)!=1 or not guess.isalpha():                                                                            
@@ -41,6 +70,13 @@ class Hangman:
             print(self.word_guessed)
     
     def play_game(self, word_list):
+        '''
+        This method will enable user to play a game of Hangman. 
+        It will test to see whether the user has either used up all their lives with incorrect guesses, or correctly guessed the secret word.
+        Arguments: word_list, which is a list of words that the secret word is selected from at random.
+        Return: the method will print out a message depending on whether the user had won or lost the game.
+
+        '''
          
         while True:
             if self.num_lives==0:
